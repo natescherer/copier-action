2384f4aca30ec77c7d4c8d7154ac0cc8e233e2e8
Nate Scherer <376408+natescherer@users.noreply.github.com>
2026-03-27 16:58:05 +0000

    feat: move action logic into discrete py files

---
+++ copier-action/validate_inputs.py
@@ -0,0 +1,22 @@
+"""Functions related to validating Action inputs."""
+
+import os
+import sys
+
+
+def validate_inputs() -> None:
+    """Validate the inputs provided to the Action."""
+    valid_subcommands = ["check-update"]
+    if not os.environ["SUBCOMMAND"]:
+        print(
+            "::error title=Copier Action Error::Input 'subcommand' "
+            "is required but was not provided"
+        )
+        sys.exit(1)
+    if os.environ["SUBCOMMAND"] not in valid_subcommands:
+        print(
+            "::error title=Copier Action Error::Value "
+            f"'{os.environ['SUBCOMMAND']}' for input 'subcommand' is not one "
+            f"of these valid values: {valid_subcommands}"
+        )
+        sys.exit(1)
