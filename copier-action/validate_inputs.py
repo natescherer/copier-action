fdb66809209c07411243291363c319b92a27617f
Nate Scherer <376408+natescherer@users.noreply.github.com>
2026-03-27 17:26:51 +0000

    fix: fix input validation logic

--- copier-action/validate_inputs.py
+++ copier-action/validate_inputs.py
@@ -1,22 +1,21 @@
-"""Functions related to validating Action inputs."""
+"""Provides validation for Action inputs."""
 
 import os
 import sys
 
+valid_subcommands = ["check-update"]
 
-def validate_inputs() -> None:
-    """Validate the inputs provided to the Action."""
-    valid_subcommands = ["check-update"]
-    if not os.environ["SUBCOMMAND"]:
-        print(
-            "::error title=Copier Action Error::Input 'subcommand' "
-            "is required but was not provided"
-        )
-        sys.exit(1)
-    if os.environ["SUBCOMMAND"] not in valid_subcommands:
-        print(
-            "::error title=Copier Action Error::Value "
-            f"'{os.environ['SUBCOMMAND']}' for input 'subcommand' is not one "
-            f"of these valid values: {valid_subcommands}"
-        )
-        sys.exit(1)
+if not os.environ["SUBCOMMAND"]:
+    print(
+        "::error title=Copier Action Error::Input 'subcommand' "
+        "is required but was not provided"
+    )
+    sys.exit(1)
+
+if os.environ["SUBCOMMAND"] not in valid_subcommands:
+    print(
+        "::error title=Copier Action Error::Value "
+        f"'{os.environ['SUBCOMMAND']}' for input 'subcommand' is not one "
+        f"of these valid values: {valid_subcommands}"
+    )
+    sys.exit(1)
