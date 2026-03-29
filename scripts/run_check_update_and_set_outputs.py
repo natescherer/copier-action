ab7d0034fde14c310f22f9a7168e460dc3681b91
Nate Scherer <376408+natescherer@users.noreply.github.com>
2026-03-29 00:20:23 +0000

    feat: make outputs functional

--- scripts/run_check_update_and_set_outputs.py
+++ scripts/run_check_update_and_set_outputs.py
@@ -2,6 +2,7 @@
 
 import json
 import os
+import sys
 from pathlib import Path
 
 from plumbum import local
@@ -9,24 +10,38 @@ from plumbum import local
 COPIER_ABSOLUTE_PATH = Path(os.environ["COPIER_ABSOLUTE_PATH"])
 INPUT_ANSWERS_FILE = os.environ["INPUT_ANSWERS_FILE"]
 INPUT_PRERELEASES = os.environ["INPUT_PRERELEASES"]
-PRERELEASES_CONVERTED = "--prereleases" if INPUT_PRERELEASES == "true" else ""
+PRERELEASES_CONVERTED = "--prereleases" if INPUT_PRERELEASES == "true" else None
 
 
 def main():
     """Runs 'copier check-update' and sets Action outputs."""
     copier = local["copier"]
 
-    copier_output = json.parse(
-        copier(
-            "check-update",
-            COPIER_ABSOLUTE_PATH,
-            "--answers-file",
-            INPUT_ANSWERS_FILE,
-            PRERELEASES_CONVERTED,
-        )
+    copier_output = copier(
+        "check-update",
+        COPIER_ABSOLUTE_PATH,
+        "--output-format",
+        "json",
+        "--answers-file",
+        INPUT_ANSWERS_FILE,
+        PRERELEASES_CONVERTED,
     )
 
-    print(copier_output)
+    try:
+        update_data = json.loads(copier_output)
+    except (json.JSONDecodeError, TypeError) as e:
+        print(
+            f"Output from copier was not the expected JSON, exiting: {e}",
+            file=sys.stderr,
+        )
+        sys.exit(1)
+
+    with Path(os.environ["GITHUB_OUTPUT"]).open("a") as f:
+        f.write(f"current-version={update_data['current_version']}\n")
+        f.write(f"latest-version={update_data['latest_version']}\n")
+        f.write(
+            f"update-available={'true' if update_data['latest_version'] else 'false'}\n"
+        )
 
 
 if __name__ == "__main__":
